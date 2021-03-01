3139141d0f74c52bf4837220d93776ab096c0c16
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2021-02-28 23:32:37 -0500

    added urls and wired the paths, to then connect with the templates.

---
+++ auth/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from auth import views
+
+urlpatterns = [
+
+    path('signup/', views.signupview, name='sign_up'),
+    path('login/', views.loginview, name='login'),
+    path('logout/', views.logout, name='logout')
+
+
+]
