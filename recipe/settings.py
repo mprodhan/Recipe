6da4facd188e18986345d2228d4871375b257384
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2021-03-21 00:45:22 -0400

    set up the settings.py media root and media files directory along with a static directory.

--- recipe/settings.py
+++ recipe/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -125,5 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join((BASE_DIR), 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join((BASE_DIR), 'media')
 
 AUTH_USER_MODEL = 'recipe_user.RecipeUser'
